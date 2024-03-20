83b05c0ae0fb3a37dd02147344fa3e2977c81e03
Ralf Stuckert <ralf.stuckert@compeople.de>
2024-03-20 12:33:08 +0100

    try to fix message receiver test on github

--- src/test/kotlin/com/github/ralfstuckert/springazuritedemo/AzuriteTestcontainer.kt
+++ src/test/kotlin/com/github/ralfstuckert/springazuritedemo/AzuriteTestcontainer.kt
@@ -3,15 +3,13 @@ package com.github.ralfstuckert.springazuritedemo
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.GenericContainer
-import org.testcontainers.junit.jupiter.Container
 
 
 class AzuriteContainer : GenericContainer<AzuriteContainer>("mcr.microsoft.com/azure-storage/azurite:3.29.0")
 
-abstract class AzuriteTestcontainer() {
+abstract class AzuriteTestcontainer()  {
     companion object {
 
-        @Container
         val container = AzuriteContainer().withExposedPorts(10000, 10001, 10002)
 
         // this is a public documented key for testing purposes with azurite, see https://learn.microsoft.com/de-de/azure/storage/common/storage-use-azurite?tabs=visual-studio%2Cblob-storage#connect-to-azurite-with-sdks-and-tools
@@ -22,7 +20,7 @@ abstract class AzuriteTestcontainer() {
             "http://${container.host}:${container.getMappedPort(10000)}"
         }
         val queueUrl by lazy {
-            "http://127.0.0.1:${container.getMappedPort(10001)}"
+            "http://${container.host}:${container.getMappedPort(10001)}"
         }
         val tableUrl by lazy {
             "http://${container.host}:${container.getMappedPort(10002)}"
@@ -39,15 +37,12 @@ abstract class AzuriteTestcontainer() {
         @JvmStatic
         @DynamicPropertySource
         fun registerPgProperties(registry: DynamicPropertyRegistry) {
-            registry.add("demo.storage.blob.url") { blobUrl }
-            registry.add("demo.storage.queue.url") { queueUrl }
-            registry.add("demo.storage.table.url") { tableUrl }
+            registry.add("demo.storage.table.endpoint") { "${tableUrl}/devstoreaccount1" }
 
             // overwrite endpoints using the testcontainers ports
             registry.add("spring.cloud.azure.storage.blob.endpoint") { "${blobUrl}/devstoreaccount1" }
             registry.add("spring.cloud.azure.storage.queue.endpoint") { "${queueUrl}/devstoreaccount1" }
         }
     }
-
 
 }
